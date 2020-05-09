6678673e312b13b80be503773a7754c46153925b
SukantaBala28 <sukantabalacste2016@gmail.com>
2020-05-09 11:46:03 +0600

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const experss = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -19,8 +20,16 @@ mongoose
 .catch(err => console.log(err));
 
 //Use Routes
-
 app.use('/api/items', items);
+
+//server static assest if in production
+if(process.env.NODE_ENV == 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000;
 
